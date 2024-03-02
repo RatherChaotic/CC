13ccaaed6c6954e05111b3cc8eb558b576681962
null <OccationallyChaotic@outlook.com>
2024-03-02 07:25:25 -0500

    Updated thor.lua
    - Now includes a list.json with many stargate address pre dialed (mostly those that are on the server)
    - Contact me if you would like to learn how to set up your own list.json

--- thor.lua
+++ thor.lua
@@ -3,7 +3,7 @@ interface, interface_type = utils.detectInterface()
 chat = peripheral.find("chatBox")
 player = peripheral.find("playerDetector")
 info = utils.getInfo(interface, interface_type)
-
+list = textutils.unserialiseJSON(http.get("https://raw.githubusercontent.com/RatherChaotic/CC/main/list.json").readAll())
 function parseCommand()
    local event, username, message, uuid, isHidden = os.pullEvent("chat")
    local parseMessage = utils.stringToTable(message) 
@@ -29,12 +29,24 @@ function dialCommand(parseMessage, username)
    if (interface) then
       if (utils.checkPlayer(10, player, username) or parseMessage[3] == tostring(os.getComputerID()) or parseMessage[3] == os.getComputerLabel()) then
          if (parseMessage[4] == nil) then
-            utils.dialAddress(utils.stringToTable(parseMessage[3], "-"), interface, interface_type)
+            if (list[parseMessage[3]] ~= nil) then
+               utils.dialAddress(list[parseMessage[3]], interface, interface_type)
+            else
+               utils.dialAddress(utils.stringToTable(parseMessage[3], "-"), interface, interface_type)
+            end
+         else
+            if (list[parseMessage[4]] ~= nil) then
+               utils.dialAddress(list[parseMessage[4]], interface, interface_type)
+            else
+               utils.dialAddress(utils.stringToTable(parseMessage[4], "-"), interface, interface_type)
+            end
+         end
+      elseif (info[4] ~= nil and parseMessage[4] ~= nil and utils.tableToString(info[4]) == utils.tableToString(utils.stringToTable(parseMessage[3], "-"))) then
+         if (list[parseMessage[4]] ~= nil) then
+            utils.dialAddress(list[parseMessage[4]], interface, interface_type)
          else
             utils.dialAddress(utils.stringToTable(parseMessage[4], "-"), interface, interface_type)
          end
-      elseif (info[4] ~= nil and parseMessage[4] ~= nil and utils.tableToString(info[4]) == utils.tableToString(utils.stringToTable(parseMessage[3], "-"))) then
-         utils.dialAddress(utils.stringToTable(parseMessage[4], "-"), interface, interface_type)
       end
    end
 end
